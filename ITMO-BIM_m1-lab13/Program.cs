58b92771fe6b3d5f223668324a8e6d58884fecea
cadman77 <cadman77@yandex.ru>
2022-01-23 00:26:12 +0300

    2nd upload attempt (after occasional delete)

---
+++ ITMO-BIM_m1-lab13/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO_BIM_m1_lab13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+/*            Triangle TR1 = new Triangle(1,2,3);
+            RectangularTriangle TR2 = new RectangularTriangle(3,4);*/
+            RegularTriangle RgTr = new RegularTriangle(5);
+            Triangle Tr = RgTr;
+            if (Tr is RectangularTriangle)
+            {
+                RectangularTriangle RcTr = (RectangularTriangle)Tr;
+            }
+            Console.ReadKey();
+        }
+    }
+    class Triangle
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        public Triangle() { }
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+        public double GetArea(double A, double B, double C)
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p*(p-A)*(p-B)*(p-C));
+        }
+    }
+    class RectangularTriangle : Triangle
+    {
+        public RectangularTriangle(double a, double b)
+        {
+            A = a;
+            B = b;
+            C = Math.Sqrt(a * a + b * b);
+        }
+    }
+    class RegularTriangle : Triangle
+    {
+        public RegularTriangle(double a)
+        {
+            A = a;
+            B = a;
+            C = a;
+        }
+    }
+}
---
+++ task/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiBuilding MBLD = new MultiBuilding("ул.Пушкина, д.1", 50, 20, 12, 3);
+            MBLD.Print();
+            Console.ReadKey();
+        }
+    }
+    class Building
+    {
+        public string Adress
+            { get; set; }
+        public double Length
+            { get; set; }
+        public double Width
+            { get; set; }
+        public double Height
+            { get; set; }
+        public Building()
+        {
+        }
+        public Building (string s, double l, double w, double h)
+        {
+            Adress = s;
+            Length = l;
+            Width = w;
+            Height = h;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Адрес здания: {Adress}");
+            Console.WriteLine($"Длина здания - {Length}");
+            Console.WriteLine($"Ширина здания - {Width}");
+            Console.WriteLine($"Высота здания - {Height}");
+        }
+    }
+    sealed class MultiBuilding : Building
+    {
+        byte TopFloor
+            { get; set; }
+        public MultiBuilding(string s, double l, double w, double h, byte tf)
+            :base(s, l, w, h)
+        {
+            TopFloor = tf;
+        }
+        public void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Этажность здания - {TopFloor}");
+        }
+    }
+}
